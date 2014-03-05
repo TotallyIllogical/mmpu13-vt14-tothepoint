ee462f56923dfd3f95c4ea4e23ae80916a50c588
Emma Andersson <eia.andersson@gmail.com>
2014-03-05 13:51:01 +0100

    Adding basic files

---
+++ include/header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<head>
+ <title><?php echo $pageTitle;?></title>
+ <link rel="stylesheet" href="style/style.css" type="text/css">
+ <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">
+</head>
+<body>
+	<h1><?php echo $pageTitle;?></h1>
+	<form class="form-wrapper cf">
+        <input type="text" placeholder="Search here..." required>
+        <button type="submit">Search</button>
+    </form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	$pageTitle = "To The Point!";
+?>
+
+
+<?php
+	include('include/header.php');
+?>
+
+<?php
+	include('include/result.php');
+?>
+
+<?php
+	include('include/footer.php');
+?>
