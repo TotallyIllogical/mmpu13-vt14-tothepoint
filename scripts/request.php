fc53979ca25acde387a58166fb17e4fecb16a470
Emma Andersson <eia.andersson@gmail.com>
2014-05-16 16:06:41 +0200

    fixed api-key

--- scripts/request.php
+++ scripts/request.php
@@ -1,6 +1,9 @@
 <?php
+
+$api_key = 'c9ec56f0f1ccf916a4baa2b711e5ce29';
+
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://api.themoviedb.org/3/search/movie/550?api_key=c9ec56f0f1ccf916a4baa2b711e5ce29/");
+curl_setopt($ch, CURLOPT_URL, "https://api.themoviedb.org/3/search/movie/550?api_key=c9ec56f0f1ccf916a4baa2b711e5ce29");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 curl_setopt($ch, CURLOPT_HEADER, FALSE);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept: application/json"));
