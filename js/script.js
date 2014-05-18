a19f326de14123836059d714864e53b791b22b39
Emma Andersson <eia.andersson@gmail.com>
2014-05-18 15:42:50 +0200

    Fixed the poster search, however the result when there aren't any posters found doens't work

--- js/script.js
+++ js/script.js
@@ -20,14 +20,14 @@ $(document).ready(function(){
             $('#poster').html("<h2 class='loading'>Your poster is on its way!</h2>");
 
             $.getJSON("http://api.themoviedb.org/3/search/movie?query=" + film + "&api_key=c9ec56f0f1ccf916a4baa2b711e5ce29", function(json) {
-              console.log(film);
+              console.log(json);
 
                if (json != "Nothing found."){
-                     $('#poster').html('<h2 class="loading">Well, gee whiz! We found you a poster, skip!</h2><img id="thePoster" src=' + json[0].poster_path + ' />');
+                     $('#poster').html('<h2 class="loading">Well, gee whiz! We found you a poster, skip!</h2><img id="thePoster" src=http://image.tmdb.org/t/p/w500/' + json.results[0].poster_path + ' />');
                   } else {
                      $.getJSON("http://api.themoviedb.org/3/search/movie?query=" + film + "&api_key=c9ec56f0f1ccf916a4baa2b711e5ce29", function(json) {
                         console.log(json);
-                        $('#poster').html('<h2 class="loading">We re afraid nothing was found for that search. Perhaps you were looking for The Goonies?</h2><img id="thePoster" src=' + json[0].poster_path + ' />');
+                        $('#poster').html('<h2 class="loading">We re afraid nothing was found for that search.</h2>');
                      });
                   }
              });
