208ab211e339061119388f61ff9c05d85e36262e
Emma Andersson <eia.andersson@gmail.com>
2014-05-20 21:39:46 +0200

    Fixed the second query

--- js/script.js
+++ js/script.js
@@ -19,22 +19,25 @@ $(document).ready(function(){
 
             $.getJSON("http://api.themoviedb.org/3/search/movie?query=" + film + "&api_key=c9ec56f0f1ccf916a4baa2b711e5ce29", function(json) {
               console.log(json);
-              
 
-               if (json != "Not Found"){
-                // Använd id från den första queryin för att hämta resten av informationen, se nedan
-                // "http://private-1487-themoviedb.apiary-proxy.com/3/movie/" + json.results[0].id + "?api_key=c9ec56f0f1ccf916a4baa2b711e5ce29"
-                // 
-                     $('#poster').html('<h2 class="loading"></h2><img id="thePoster" src=http://image.tmdb.org/t/p/w500/' + json.results[0].poster_path + ' />');
-                     $('#title').html('<h3>' + json.results[0].title + '</h3>');
-                     $('#release').html(json.results[0].release_date);
-                     $('#score').html(json.results[0].vote_average);
-                  } else {
-                     $.getJSON("http://api.themoviedb.org/3/search/movie?query=" + film + "&api_key=c9ec56f0f1ccf916a4baa2b711e5ce29", function(json) {
-                        console.log(json);
-                        $('#poster').html('<h2 class="loading">We are afraid nothing was found for that search.</h2>');
-                     });
-                  }
+              if (json != "Not Found"){
+                // Använd id från den första queryin för att hämta resten av informationen från en annan json, se nedan
+                var movieid= json.results[0].id;
+        
+                $.getJSON("https://api.themoviedb.org/3/movie/" + movieid + "?api_key=c9ec56f0f1ccf916a4baa2b711e5ce29", function(json) {
+                  console.log(json);
+                  $('#poster').html('<h2 class="loading"></h2><img id="thePoster" src=http://image.tmdb.org/t/p/w500/' + json.poster_path + ' />');
+                  $('#title').html('<h3>' + json.title + '</h3>');
+                  $('#release').html(json.release_date);
+                  $('#score').html(json.vote_average);
+
+                  });
+                } else {
+                   $.getJSON("http://api.themoviedb.org/3/search/movie?query=" + film + "&api_key=c9ec56f0f1ccf916a4baa2b711e5ce29", function(json) {
+                      console.log(json);
+                      $('#poster').html('<h2 class="loading">We are afraid nothing was found for that search.</h2>');
+                   });
+                }
              });
 
           }
