9f7988aaca5c2a6c6b68a9dc7599cf8fa93e5b5a
Emma Andersson <eia.andersson@gmail.com>
2014-05-21 18:56:23 +0200

    Stupid code not doing what I asked to to

--- js/script.js
+++ js/script.js
@@ -23,20 +23,24 @@ $(document).ready(function(){
                 // Använd id från sök-api:n för att hämta resten av informationen med hjälp av huvud-api:n, se nedan
                 var movieid= json.results[0].id;
                 $.getJSON("https://api.themoviedb.org/3/movie/" + movieid + "?api_key=c9ec56f0f1ccf916a4baa2b711e5ce29", function(json) {
-                  console.log(json); //console logar json för att se hur den ser ut för att hjälpa mig hämta ut datan
 
                   $('#poster').html('<h2 class="loading"></h2><img id="thePoster" src=http://image.tmdb.org/t/p/w500/' + json.poster_path + ' />'); //Här behövs det en if-sats som hämtar en placeholder bild ifall den poster inte hittas
 
                   $('#title').html('<h3>' + json.title + '</h3>');
                   // Här ska alternativ titlar skrivas ut
                   $('#description').html(json.overview + '<br>&raquo; <a href="https://www.themoviedb.org/movie/' + movieid + '">Read more</a>'); //Här behöver vi sätta in så "read more" länken öppnar en ny sida
-                  $('#genre').html(json.genres[0].name); //behövs en foreach loop här eftersom de flesta filmer har med än en genre
+                  $('#genre').html(json.genres[0].name); //behövs en loop här eftersom de flesta filmer har med än en genre
                   $('#release').html(json.release_date);
-                  $('#language').html(json.spoken_languages[0].name); //behövs en foreach loop här eftersom en film kan har flera språk
+                  $('#language').html(json.spoken_languages[0].name); //behövs en loop här eftersom en film kan har flera språk
                   $('#runtime').html(json.runtime);
                   $('#score').html(json.vote_average);
 
-                  });
+                });
+                // Den här biten fungerar inte
+                $.getJSON("https://api.themoviedb.org/3/movie/" + movieid + "casts?api_key=c9ec56f0f1ccf916a4baa2b711e5ce29", function(json2) {
+                  console.log(json2);
+                  $('#starring').html(json2[0].name); //Här behövs en loop för att hämta antalet skådespelarnamn du önskar
+                });
                 } else {
                    $.getJSON("http://api.themoviedb.org/3/search/movie?query=" + film + "&api_key=c9ec56f0f1ccf916a4baa2b711e5ce29", function(json) {
                       console.log(json);
