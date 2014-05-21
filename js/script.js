e35c7d8c56e12ee79e5c4f048aaaa866dbf597cb
Emma Andersson <eia.andersson@gmail.com>
2014-05-21 21:56:26 +0200

    Fixed the result page

--- js/script.js
+++ js/script.js
@@ -22,6 +22,10 @@ $(document).ready(function(){
             $.getJSON("http://api.themoviedb.org/3/search/movie?query=" + film + "&api_key=c9ec56f0f1ccf916a4baa2b711e5ce29", function(json) {
 
               if (json.total_results != 0 || json.total_pages != 0){
+
+                // Populera result-rutan (Dear f-ing Bob, the amount of code...)
+                $('#result').html('<div class="row"><div class="large-12 columns"><div class="panel" id="wrapper"><div class="row" id="test"><div class="large-3 medium-3 columns" id="poster"><section id="poster"></section></div><div class="large-5 medium-5 columns"><section id="title"></section><p id="description"></p></div><div class="large-4 medium-4 columns"><h5><a href="#">Information</a></h5><strong>Genre:</strong> <section id="genre"></section><strong>Released:</strong> <section id="release"></section><strong>Language:</strong> <section id="language"></section><strong>Runtime:</strong> <section id="runtime"></section><strong>Director:</strong> <section id="director"></section><strong>Writer:</strong> <section id="writer"></section><strong>Starring:</strong> <section id="starring"></section></div></div><!-- .row --><div class="row"><div class="large-12 columns"><table class="responsive" id="table-wrapper"><thead><tr><th class="center">Source</th><th class="center"><a href="http://www.themoviedb.org/" target="_blank"><img src="img/tmdb.png"></a></th><th class="center"><a href="http://www.imdb.com/" target="_blank"><img src="img/imdb.png"></a></th><th class="center"><a href="http://www.rottentomatoes.com/" target="_blank"><img src="img/rottentomatoes.png"></a></th></tr></thead><tbody><tr><td class="center"><strong>Maximum</strong></td><td class="center">10</td><td class="center">10</td><td class="center">100%</td></tr><tr><td class="center"><strong>Rating</strong></td><td class="rating"><section id="score"></section></td><td class="rating">8.5</td><td class="rating">93%</td></tr></tbody></table></div></div></div></div>d</div>')
+
                 console.log(json);
                 // Använd id från sök-api:n för att hämta resten av informationen med hjälp av huvud-api:n, se nedan
                 var movieid= json.results[0].id;
@@ -45,8 +49,8 @@ $(document).ready(function(){
                 $.getJSON("https://api.themoviedb.org/3/movie/" + movieid + "/casts?api_key=c9ec56f0f1ccf916a4baa2b711e5ce29", function(json) {
                   
                   $('#starring').html(json.cast[0].name); //Här behövs en loop för att hämta antalet skådespelarnamn du vill visa
-                  $('#writer').html(); //Här behövs en if/when sats där ett namn hämtas om "job" matchar writer
-                  $('#director').html(); //Här behövs en if/when sats där ett namn hämtas om "job" matchar director
+                  $('#writer').html(); //Här behövs en if-sats där ett namn hämtas om "job" matchar writer
+                  $('#director').html(); //Här behövs en if-sats där ett namn hämtas om "job" matchar director
 
                 });
 
