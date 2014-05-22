97241929cb44ec334d658a2aa255792ced030ea8
Hannele <paivi.hannele@gmail.com>
2014-05-22 21:18:05 +0200

    Added titles to links in footer.

--- include/footer.php
+++ include/footer.php
@@ -1,15 +1,15 @@
     <div class="row">
-		<div class="large-12 columns">
+        <div class="large-12 columns">
 
                 <p id="footer">Designed & Developed by Hanni & Eia 2014</p>
-			
-    			<p id="footer">This product uses the TMDb API but is not endorsed or certified by 
-    				<a href="http://www.themoviedb.org/" target="_blank">TMDb</a><br><br>
-                    <a href="http://www.themoviedb.org/"><img src="img/tmdb-logo.png" alt="The Movie Database"></a>
-    	        </p>
+            
+                <p id="footer">This product uses the TMDb API but is not endorsed or certified by 
+                    <a href="http://www.themoviedb.org/" target="_blank" title="The Movie Database">TMDb</a><br><br>
+                    <a href="http://www.themoviedb.org/"><img src="img/tmdb-logo.png" title="The Movie Database"></a>
+                </p>
 
-    	</div>
-	</div>
+        </div>
+    </div>
     
     <script src="js/vendor/jquery.js"></script>
     <script src="js/foundation.min.js"></script>
