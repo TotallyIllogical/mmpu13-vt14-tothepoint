7676f3569407646710fb2e32108f177c168ad926
Emma Andersson <eia.andersson@gmail.com>
2014-05-24 16:16:08 +0200

    Made an $.each to fetch director and writers

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
               if (json.total_results != 0 || json.total_pages != 0){
 
                 // Populera result-rutan (Dear f-ing Bob, the amount of code...)
-                $('#result').html('<div class="row"><div class="large-12 columns"><div class="panel" id="wrapper"><div class="row"><div class="large-3 medium-3 columns" id="poster"></div><div class="large-5 medium-5 columns"><section id="title"></section><section id="orgTitle"></section><p id="description"></p></div><div class="large-4 medium-4 columns"><h5><a href="#">Information</a></h5><strong>Genre:</strong> <section id="genre"></section><strong>Released:</strong> <section id="release"></section><strong>Language:</strong> <section id="language"></section><strong>Runtime:</strong> <section id="runtime"></section><strong>Director:</strong> <section id="director"></section><strong>Writer:</strong> <section id="writer"></section><strong>Starring:</strong> <section id="starring"></section></div></div><!-- .row --><div class="row"><div class="large-12 columns"><table class="responsive" id="table-wrapper"><thead><tr><th class="center">Source</th><th class="center"><a href="http://www.themoviedb.org/" target="_blank"><img src="img/tmdb.png"></a></th><th class="center"><a href="http://www.imdb.com/" target="_blank"><img src="img/imdb.png"></a></th><th class="center"><a href="http://www.rottentomatoes.com/" target="_blank"><img src="img/rottentomatoes.png"></a></th></tr></thead><tbody><tr><td class="center"><strong>Maximum</strong></td><td class="center">10</td><td class="center">10</td><td class="center">100%</td></tr><tr><td class="center"><strong>Rating</strong></td><td class="rating" id="score"></td><td class="rating">8.5</td><td class="rating">93%</td></tr></tbody></table></div></div></div></div>d</div>')
+                $('#result').html('<div class="row"><div class="large-12 columns"><div class="panel" id="wrapper"><div class="row" id="test"><div class="large-3 medium-3 columns" id="poster"></div><div class="large-5 medium-5 columns"><section id="title"></section><p id="description"></p></div><div class="large-4 medium-4 columns"><h5><a href="#">Information</a></h5><strong>Genre:</strong> <section id="genre"></section><strong>Released:</strong> <section id="release"></section><strong>Language:</strong> <section id="language"></section><strong>Runtime:</strong> <section id="runtime"></section><strong>Director:</strong> <section id="director"></section><strong>Writer:</strong> <section id="writer"></section><strong>Starring:</strong> <section id="starring"></section></div></div><!-- .row --><div class="row"><div class="large-12 columns"><table class="responsive" id="table-wrapper"><thead><tr><th class="center">Source</th><th class="center"><a href="http://www.themoviedb.org/" target="_blank"><img src="img/tmdb.png"></a></th><th class="center"><a href="http://www.imdb.com/" target="_blank"><img src="img/imdb.png"></a></th><th class="center"><a href="http://www.rottentomatoes.com/" target="_blank"><img src="img/rottentomatoes.png"></a></th></tr></thead><tbody><tr><td class="center"><strong>Maximum</strong></td><td class="center">10</td><td class="center">10</td><td class="center">100%</td></tr><tr><td class="center"><strong>Rating</strong></td><td class="rating" id="score"></td><td class="rating">8.5</td><td class="rating">93%</td></tr></tbody></table></div></div></div></div>d</div>')
 
                 
                 // Använd id från sök-api:n för att hämta resten av informationen med hjälp av huvud-api:n, se nedan
@@ -35,23 +35,12 @@ $(document).ready(function(){
 
                   $('#poster').html('<h2 class="loading"></h2><img id="thePoster" src=http://image.tmdb.org/t/p/w500/' + json.poster_path + ' />'); //Här behövs det en if-sats som hämtar en placeholder bild ifall den poster inte hittas
 
-                  $('#title').html('<h2>' + json.title + '</h2>');
-
-                  // Hämtar originaltitel  
-                  if (json.title != json.original_title) {
-                    $('#orgTitle').html('<h5>' + json.original_title + ' <em>(original title)</em>' + '</h5>');
-                  }                                                                                              
-                  
+                  $('#title').html('<h3>' + json.title + '</h3>');
+                  // Här ska alternativ titlar skrivas ut                                                                                 
                   var words = json.overview;                
-                  words = words.substr(0,320);
-                  
-                  if (words.length == 320) {
-                    words += '...';
-                  };
-
+                  words = words.substr(0,370);
                   $("#description").html(words + '<br>&raquo; <a href="https://www.themoviedb.org/movie/' + movieid + '" target="_blank">Read more</a>');
-
-
+                  
                   $.each(json.genres, function( index, value ) {
                     $( "#genre" ).append( document.createTextNode( value.name + ", " ) );
                   });
@@ -70,9 +59,16 @@ $(document).ready(function(){
                 $.getJSON("https://api.themoviedb.org/3/movie/" + movieid + "/casts?api_key=c9ec56f0f1ccf916a4baa2b711e5ce29", function(json) {
                   
                   $('#starring').html(json.cast[0].name); //Här behövs en loop för att hämta antalet skådespelarnamn du vill visa
-                  $('#writer').html(); //Här behövs en if-sats där ett namn hämtas om "job" matchar writer
-                  $('#director').html(); //Här behövs en if-sats där ett namn hämtas om "job" matchar director
-                  console.log(json);
+                  
+                  $.each(json.crew, function( index, value ) {
+                    console.log(value);
+                    if(value.job == "Writer" || value.job == "Author" || value.job == "Screenplay"){
+                      $( "#writer" ).append( document.createTextNode( value.name + ", " ) );
+                    }
+                    if(value.job == "Director"){
+                      $( "#director" ).append( document.createTextNode( value.name ) );
+                    }
+                  });
 
                 });
 
