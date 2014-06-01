4c784823f1d47e46e95b247b1d878fd817d7d5e4
Emma Andersson <eia.andersson@gmail.com>
2014-06-01 17:45:12 +0200

    Finally fixed the broken Bob's

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-// Vi behöver skriva så att när man gör en ny sökning så resettar resultat-rutan
-
 $(document).ready(function(){
 
+  // Sökbar funktionen
   $('#term').focus(function(){
     var full = $(".result").has("img").length ? true : false;
     if(full == false){
@@ -18,133 +17,180 @@ $(document).ready(function(){
       $('.result').html('<div class="row"><div class="large-12 columns"><div class="panel wrapper"><div class="row"><div class="large-12 columns"><h3>Please type something in the searchfield.</h3></div></div></div></div></div>');
 
     } else {
-      //Använder tmdb sök-api för att hitta filmer vars titel matchar det som eftersöks
-    
-      $.getJSON("http://api.themoviedb.org/3/search/movie?query=" + film + "&api_key=c9ec56f0f1ccf916a4baa2b711e5ce29", function(json) {
+      
+      // Rensar Bob
+      $(".result").empty();
 
+      // Använder tmdb sök-api för att hitta filmer vars titel matchar det som eftersöks
+      $.getJSON("http://api.themoviedb.org/3/search/movie?query=" + film + "&api_key=c9ec56f0f1ccf916a4baa2b711e5ce29", function(json) {
+        console.log(json);
+        // Kollar så att results eller pages inte är tomma
         if (json.total_results != 0 || json.total_pages != 0){
 
-          // Använd idnummer från sök-api:n för att hämta resten av informationen med hjälp av huvud-api:n
-          for(var i = 0; i < 5; i++){
-          var movieid = json.results[i].id;
-          console.log(movieid);
-          // Populera result-rutan (Dear f-ing Bob, the amount of code...)
-          // .html ersätter det som står, append lägger till. Det var därför loopen inte fungerade, vi hade gjort rätt.
-          $('.result').append('<div id="'+i+'" class="row"><div class="large-12 columns"><div class="panel wrapper"><div class="row"><div class="large-4 medium-4 columns poster"></div><article class="large-8 medium-8 columns"><h2 class="title"></h2><h3 class="org-title orgTitle"></h3><p class="description"></p><h4>Information</h4><strong>Language:</strong> <div class="info language"></div><strong>Released:</strong> <div class="info release"></div><strong>Runtime:</strong> <div class="info runtime"></div><br><strong>Genre:</strong> <div class="info genre"></div><br><strong>Director:</strong> <div class="info director"></div><br><strong>Writer:</strong> <div class="info writer"></div><br><strong>Starring:</strong> <div class="info starring"></div><div class="buttons-wrapper"><div class="read-more"></div></div></article><!-- .8-columns --></div><!-- .row --><div class="row"><div class="large-12 columns"><table class="responsive table-wrapper"><thead><tr><th class="center strong">Source</th><th class="center"><a href="http://www.themoviedb.org/" target="_blank"><img src="img/tmdb.png" alt="The Movie Database" class="logo"></a></th> <th class="center"><a href="http://www.imdb.com/" target="_blank"><img src="img/imdb.png" alt="IMDb" class="logo"></a></th><th class="center"><a href="http://www.rottentomatoes.com/" target="_blank"><img src="img/rottentomatoes.png" alt="Rotten Tomatoes" class="logo"></a></th></tr></thead><tbody><tr><td class="center strong">Maximum</td><td class="center">10</td><td class="center">10</td><td class="center">100%</td></tr><tr><td class="center strong">Rating</td><td class="rating center tmdb-score"></td><td class="rating center imdb-score"></td><td class="rating center rt-score"></td></tr></tbody></table></div><!-- .row --></div><!-- .large-12 columns --></div><!-- .panel-wrapper --></div><!-- .large-12 columns --></div><!-- .row -->');
- 
-          // Den här getJSON hämtar basinformation så som titel, synopsis och poster
-          $.getJSON("https://api.themoviedb.org/3/movie/" + movieid + "?api_key=c9ec56f0f1ccf916a4baa2b711e5ce29", function(json) {
+          // Gör det vi vill nedan 5 gånger
+          for(var i = 0; i < 7; i++){
 
-            // Den här jsonen har imdbs id för filmen, json.imdb_id;
+            // Sätter den nuvarande filmens id nummer som variablen movieid
+            var movieid = json.results[i].id;
 
-            // Hämtar poster eller movie-placeholder-image
-            if (json.poster_path) {
-              $('#'+i).find('.poster').html('<h2 class="loading"></h2><img id="thePoster" src=http://image.tmdb.org/t/p/w500/' + json.poster_path + ' />');
-            } else {
-              $('#'+i).find('.poster').html('<h2 class="loading"></h2><img id="thePlaceHolderImg" src=img/movie-placeholder.png' + ' />');
-            }
+            // Populera result-rutan (Dear f-ing Bob, the amount of code...)
+            $('.result').append('<div id="'+movieid+'" class="row"><div class="large-12 columns"><div class="panel wrapper"><div class="row"><div class="large-4 medium-4 columns poster"></div><article class="large-8 medium-8 columns"><h2 class="title"></h2><h3 class="org-title orgTitle"></h3><p class="description"></p><h4>Information</h4><strong>Language:</strong> <div class="info language"></div><strong>Released:</strong> <div class="info release"></div><strong>Runtime:</strong> <div class="info runtime"></div><br><strong>Genre:</strong> <div class="info genre"></div><br><strong>Director:</strong> <div class="info director"></div><br><strong>Writer:</strong> <div class="info writer"></div><br><strong>Starring:</strong> <div class="info starring"></div><div class="buttons-wrapper"><div class="read-more"></div></div></article><!-- .8-columns --></div><!-- .row --><div class="row"><div class="large-12 columns"><table class="responsive table-wrapper"><thead><tr><th class="center strong">Source</th><th class="center"><a href="http://www.themoviedb.org/" target="_blank"><img src="img/tmdb.png" alt="The Movie Database" class="logo"></a></th> <th class="center"><a href="http://www.imdb.com/" target="_blank"><img src="img/imdb.png" alt="IMDb" class="logo"></a></th><th class="center"><a href="http://www.rottentomatoes.com/" target="_blank"><img src="img/rottentomatoes.png" alt="Rotten Tomatoes" class="logo"></a></th></tr></thead><tbody><tr><td class="center strong">Maximum</td><td class="center">10</td><td class="center">10</td><td class="center">100%</td></tr><tr><td class="center strong">Rating</td><td class="rating center tmdb-score"></td><td class="rating center imdb-score"></td><td class="rating center rt-score"></td></tr></tbody></table></div><!-- .row --></div><!-- .large-12 columns --></div><!-- .panel-wrapper --></div><!-- .large-12 columns --></div><!-- .row -->');
+   
+            // Den här getJSON hämtar basinformation så som titel, synopsis och poster
+            $.getJSON("https://api.themoviedb.org/3/movie/" + movieid + "?api_key=c9ec56f0f1ccf916a4baa2b711e5ce29", function(json) {
+              
+              // Definerar om variablen movieid till den nuvarandre idn
+              movieid = json.id;
 
-            $('#'+i).find('.title').append('<h3>' + json.title + '</h3>');
-
-            // Hämtar originaltitel
-            if (json.title != json.original_title) {
-              $('#'+i).find('.orgTitle').html('<h5>' + json.original_title + ' <em>(original title)</em>' + '</h5>');
-            }
-                                                                                                 
-            var words = json.overview;
-            words = words.substr(0,370);
-            if(words.length == 370){
-              words += "...";
-            }
-
-            $('#'+i).find('.description').html(words);
-
-            $('#'+i).find('.read-more').html( ' <a href="https://www.themoviedb.org/movie/' + movieid + '" class="button" target="_blank">Read more &raquo;</a>');
-
-            var languages = [];
-            $.each(json.spoken_languages, function( index, value ) {
-              languages.push(value.name);
-              return languages;
-            });
-            var languagelist = languages.join(", ");
-            $( '#'+i).find('.language' ).append( document.createTextNode(languagelist));
-
-            $('#'+i).find('.release').html(json.release_date);
-
-            $('#'+i).find('.runtime').html(json.runtime + " min");
-
-            var genres = [];
-            $.each(json.genres, function( index, value ) {
-              genres.push(value.name);
-              return genres;
-            });
-            var genrelist = genres.join(" / ");
-            $('#'+i).find('.genre').append( document.createTextNode( genrelist ) );
-
-            $('#'+i).find('.tmdb-score').html(json.vote_average);
-
-            $.getJSON("http://www.omdbapi.com/?i=" + json.imdb_id, function(json){
-              $('#'+i).find('.imdb-score').html(json.imdbRating);
-            });
-
-            var imdbIDwithLetters = json.imdb_id;
-            var theImdbId = imdbIDwithLetters.substring(2);
-
-            $.ajax({
-              url: "http://api.rottentomatoes.com/api/public/v1.0/movie_alias.json?apikey=hsvze4vnd8ks2kptercdh6sq&type=imdb&id=" + encodeURI(theImdbId),
-              dataType: "jsonp",
-              success: rtScoring
-            });
-
-            function rtScoring(data) {
-              var theRTscore = data.ratings.critics_score;
-              $('#'+i).find('.rt-score').html(theRTscore + '%');
-            }; 
-
-          });
-          // Den här getJSON hämtar rollistan, manusförfattare och regisör
-          $.getJSON("https://api.themoviedb.org/3/movie/" + movieid + "/casts?api_key=c9ec56f0f1ccf916a4baa2b711e5ce29", function(json) {
-            var directors = [];
-            var writers =[];
-
-            $.each(json.crew, function( index, value ) {
-              if(value.job == "Director"){
-                directors.push(value.name);
+              // Hämtar poster eller movie-placeholder-image
+              if (json.poster_path) {
+                $('#'+movieid).find('.poster').html('<h2 class="loading"></h2><img id="thePoster" src=http://image.tmdb.org/t/p/w500/' + json.poster_path + ' />');
+              } else {
+                $('#'+movieid).find('.poster').html('<h2 class="loading"></h2><img id="thePlaceHolderImg" src=img/movie-placeholder.png' + ' />');
               }
 
-              if(value.job == "Writer"){
-                writers.push(value.name);
+              // Lägger in titeln
+              $('#'+movieid).find('.title').append('<h3>' + json.title + '</h3>');
+
+              // Kollar om orginal titel finns och skriver ut den i sådanna fall
+              if (json.title != json.original_title) {
+                $('#'+movieid).find('.orgTitle').html('<h5>' + json.original_title + ' <em>(original title)</em>' + '</h5>');
               }
-              if(writers.length < 2){
-                if(value.job == "Author"){
+              
+              // Tar in overview och kortar ner den om den är mer än 370 tecken                            
+              var words = json.overview;
+              words = words.substr(0,370);
+
+              // Sätter ditt tre punkter om texten är 370 tecken lång
+              if(words.length == 370){
+                words += "...";
+              }
+
+              // Lägger in den nedkortade overviewn som vi döpt om till words
+              $('#'+movieid).find('.description').html(words);
+
+              // Lägger till "read more"-knappen och länkar den
+              $('#'+movieid).find('.read-more').html( ' <a href="https://www.themoviedb.org/movie/' + movieid + '" class="button" target="_blank">Read more &raquo;</a>');
+
+              // Skapar tom array
+              var languages = [];
+              // Lägger in värdena från spoken_languages i den tomma arrayen vi skapade innan
+              $.each(json.spoken_languages, function( index, value ) {
+                languages.push(value.name);
+              });
+              // Gör om arrayen till en sträng och döper om den
+              var languagelist = languages.join(", ");
+              // Skriver vi ut languagelist
+              $( '#'+movieid).find('.language' ).append( document.createTextNode(languagelist));
+
+              // Hämtar ut release date
+              $('#'+movieid).find('.release').html(json.release_date);
+
+              // Hämtar ut speltiden
+              $('#'+movieid).find('.runtime').html(json.runtime + " min");
+
+              // Skapar en tom array
+              var genres = [];
+              // Lägger in värdena i den tomma arrayen vi skapade raden ovanför
+              $.each(json.genres, function( index, value ) {
+                genres.push(value.name);
+                return genres;
+              });
+              // Gör om innehållet i arrayen till en sträng
+              var genrelist = genres.join(" / ");
+              // Lägger in genrena
+              $('#'+movieid).find('.genre').append( document.createTextNode( genrelist ) );
+
+              // Hämtar ut filmens tmdb-poäng
+              $('#'+movieid).find('.tmdb-score').html(json.vote_average);
+
+              // Hämtar en ny json från en annan källa med hjälp av imdb_id:t från den förra json
+              $.getJSON("http://www.omdbapi.com/?i=" + json.imdb_id, function(json){
+                // Hämtar ut filmens imdb-poäng
+                $('#'+movieid).find('.imdb-score').html(json.imdbRating);
+              });
+
+              // Döper om json.imdb_id
+              var imdbIDwithLetters = json.imdb_id;
+              console.log(json.imdb_id);
+              // Tar bort de första två tecknena i värdet
+              var theImdbId = imdbIDwithLetters.substring(2);
+
+              // Skapar en jsonp utifrån RT's (Rotten Tomatoes) api och om den är en "success" så kör den funktionen rtScoring
+              $.ajax({
+                url: "http://api.rottentomatoes.com/api/public/v1.0/movie_alias.json?apikey=hsvze4vnd8ks2kptercdh6sq&type=imdb&id=" + encodeURI(theImdbId),
+                dataType: "jsonp",
+                success: rtScoring
+              });
+
+              // Tar in parameten datan från ajax-hämtninen
+              function rtScoring(data) {
+                // Döper om det vi vill använda
+                var theRTscore = data.ratings.critics_score;
+                // Skriver ut filmens RT-poäng
+                $('#'+movieid).find('.rt-score').html(theRTscore + '%');
+              };
+
+            });
+            // Den här getJSON hämtar rollistan, manusförfattare och regisör
+            $.getJSON("https://api.themoviedb.org/3/movie/" + movieid + "/casts?api_key=c9ec56f0f1ccf916a4baa2b711e5ce29", function(json) {
+
+              // Skapar massa tomma arrayer
+              var directors = [];
+              var writers =[];
+              var movieid = json.id;
+
+              // Går igenom alla rader i crew och tar de som matchar
+              $.each(json.crew, function( index, value ) {
+                if(value.job == "Director"){
+                  directors.push(value.name);
+                }
+
+                // Här var det för krångligt för att lista ut hur jag skulle begränsa antalet 
+                if(value.job == "Writer" && writers.length < 2){
                   writers.push(value.name);
                 }
-              }
-              if(writers.length < 2){
-                if(value.job == "Screenplay"){
-                  writers.push(value.name);
+                if(writers.length < 2){
+                  if(value.job == "Author"){
+                    writers.push(value.name);
+                  }
                 }
+                if(writers.length < 2){
+                  if(value.job == "Screenplay"){
+                    writers.push(value.name);
+                  }
+                }
+              });
+
+              // Gör om directors arrayen till en sträng 
+              var directorlist = directors.join(", ");
+              // Hämtar ut directors 
+              $('#'+movieid).find('.director').append(document.createTextNode(directorlist));
+
+              // Kollar om writers arrayen är tom
+              if(writers.length == 0){
+                writers.push("No name found");
               }
+              // Gör om arrayen till en sträng
+              var writerslist = writers.join(", ");
+              // Skriver ut strängen
+              $( '#'+movieid).find('.writer' ).append(document.createTextNode(writerslist));
+
+              // Skapar en tom array
+              var actornames = [];
+              // Hämtar ut fem namn och pushar in dem i den tomma arrayen
+              for(var i = 0; i < 5; i++){
+                actornames.push(json.cast[i].name);
+              }
+              // Gör om arrayen till en sträng
+              var actorlist = actornames.join(", ");
+              // Skriver ut strängen
+              $('#'+movieid).find('.starring').append(document.createTextNode(actorlist));
             });
-
-            var directorlist = directors.join(", ");
-            $('#'+i).find('.director').append( document.createTextNode(directorlist));
-
-            if(writers.length == 0){
-              writers.push("No name found");
-            }
-            var writerslist = writers.join(", ");
-            $( '#'+i).find('.writer' ).append( document.createTextNode(writerslist));
-
-            var actornames = [];
-            for(var i = 0; i < 5; i++){
-              actornames.push(json.cast[i].name);
-            }
-            var actorlist = actornames.join(", ");
-            $('#'+i).find('.starring').append( document.createTextNode( actorlist ));
-          });
-        };
+          };
         } else {
+
+          // Om inga resultar hittas så skrivs detta ut
           $('.result').html('<div class="row"><div class="large-12 columns"><div class="panel wrapper"><div class="row"><div class="large-12 columns"><h3>We are afraid nothing was found for that search.</h3></div></div></div></div></div>');
         }
       });
